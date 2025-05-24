4c4a89bbc4913ff4e7a98819690f3aa5a90b715b
ahuge <nonaudh@gmail.com>
2025-05-24 15:02:30 +0200

    almost finished

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,6 +5,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=ahuge --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install --url=ahuge.42.fr --title=inception --admin_user=megatron --admin_password=megapass --admin_email=megatron@42.com --allow-root
+./wp-cli.phar user create ahuge ahuge@42.com --role=subscriber --user_pass=ahugepass --allow-root
 
 php-fpm7.4 -F
