85bd6f33ed9c7286273728762430a0fb6e8ff98e
ahuge <ahuge@debian-12.10.0.myguest.virtualbox.org>
2025-07-03 13:04:11 +0200

    jadore la vie

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
+mysql_install_db
+mysqld
+
 # service mysql start
 
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${USER}\`@'%' IDENTIFIED BY '${WP_USER_PASS}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${USER}\`@'%';"
 # mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # mysql -e "FLUSH PRIVILEGES;"
 # mysqladmin -u root -p ${SQL_ROOT_PASSWORD} shutdown
 
 # exec mysqld_safe
 
-# Initialise le répertoire de données MariaDB
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# # Initialise le répertoire de données MariaDB
+# mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-# Démarre MariaDB en mode bootstrap
-mariadbd --user=mysql --bootstrap <<EOF
-USE mysql;
-FLUSH PRIVILEGES;
+# # Démarre MariaDB en mode bootstrap
+# mariadbd --user=mysql --bootstrap <<EOF
+# USE mysql;
+# FLUSH PRIVILEGES;
 
-# Supprimez l'utilisateur et la base de données par défaut
-DROP USER IF EXISTS ''@'localhost';
-DROP DATABASE IF EXISTS test;
+# # Supprimez l'utilisateur et la base de données par défaut
+# DROP USER IF EXISTS ''@'localhost';
+# DROP DATABASE IF EXISTS test;
 
-# Créez la base de données WordPress et l'utilisateur
-CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
-CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';
+# # Créez la base de données WordPress et l'utilisateur
+# CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
+# CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
+# GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';
 
-# Changez le mot de passe de l'utilisateur root
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
-FLUSH PRIVILEGES;
-EOF
+# # Changez le mot de passe de l'utilisateur root
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+# FLUSH PRIVILEGES;
+# EOF
 
-# Exécute MariaDB
-exec mariadbd
+# # Exécute MariaDB
+# exec mariadbd
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,7 +9,7 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=ahuge --dbpass=dp_pass --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=${URL} --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 ./wp-cli.phar user create ${USER} ${USER}@42.com --role=subscriber --user_pass=${WP_USER_PASS} --allow-root
 
